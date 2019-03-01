06f87d944f4278da91219550d0b6fba777acc93f
trentwalradt <tjw2001@med.cornell.edu>
2019-03-01 11:53:58 -0500

    updated PrepareCov tests

--- tests/testthat/test_fragCounter.R
+++ tests/testthat/test_fragCounter.R
@@ -64,7 +64,7 @@ test_that("PrepareCov", {
   expect_error(PrepareCov(bam = NULL, cov = NULL))
   expect_equal(length(PrepareCov(example_bam)), 15509063)
   expect_equal(max(width(PrepareCov(example_bam))), 200)
-  expect_equal(length(PrepareCov(example_bam, exome = TRUE, skeleton = skeleton, use.skel = TRUE)), 314827)
+  expect_equal(length(PrepareCov(example_bam, exome = TRUE, skeleton = skeleton, use.skel = TRUE)), 194880)
 #  expect_equal(length(PrepareCov(example_bam, paired = FALSE)), 15509063)
 #  expect_equal(max(width(PrepareCov(example_bam, paired = FALSE))), 200)
 
